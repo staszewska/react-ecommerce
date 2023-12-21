aada4c6aa91d1cb30b67a2ddeb4a47dc1bd5b882
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2023-12-21 21:37:50 +0100

    use Link by react-router instead of Anchor element

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,19 +1,31 @@
+import { Badge } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function TopBar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">React E-Commerce App</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            React E-Commerce App
+          </Navbar.Brand>
 
           <Nav className="me-auto">
-            <Nav.Link href="#">Products</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Products
+            </Nav.Link>
+
             <Nav.Link href="#">Categories</Nav.Link>
+
             <Nav.Link href="#">Login</Nav.Link>
-            <Nav.Link href="#">Shopping Cart</Nav.Link>
+
+            <Nav.Link as={Link} to="/s">
+              Shopping Cart &nbsp;
+              <Badge bg="danger">1</Badge>
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
