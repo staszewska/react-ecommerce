4622779ef6dca3d1a993c99399b7cec99cd5a274
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2023-12-21 20:57:44 +0100

    add route for ShoppingCart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />}>
         <Route path="/" element={<ProductList />} />
         <Route path="/p/:id" element={<ProductDetails />} />
+        <Route path="/s" element={<ShoppingCart />} />
       </Route>
     </Routes>
   );
---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,9 @@
+function ShoppingCart() {
+  return (
+    <section>
+      <h2>Shopping Cart</h2>
+    </section>
+  );
+}
+
+export default ShoppingCart;
