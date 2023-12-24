1cda244c728bc310dfd1d4ab684dbbf3db9ceb8b
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2023-12-24 02:07:39 +0100

    add global store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import TopBar from '../components/TopBar';
 import NavigationTree from '../components/NavigationTree';
-import {
-  ShoppingCartContext,
-  DispatchShoppingCartContext,
-  initialShoppingCart,
-  shoppingCartReducer,
-} from '../contexts/ShoppingCartContext';
-import { useReducer } from 'react';
 import { StoreProvider } from 'easy-peasy';
-import store from '../stores/shoppingCart';
+import store from '../utils/store';
 
 function Home() {
-  const [shoppingCart, dispatchShoppingCart] = useReducer(shoppingCartReducer, initialShoppingCart);
-
   return (
     <StoreProvider store={store}>
       <header>
