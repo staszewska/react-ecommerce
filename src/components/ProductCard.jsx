669f1adbb304e1404f23271e448382378f0f5d39
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-01-03 22:49:05 +0100

    use Link component for ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,21 @@
 import { Link } from 'react-router-dom';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { Rating } from 'react-simple-star-rating';
 
 function ProductCard({ product }) {
   return (
     <Card key={product.id} style={{ width: '10rem' }}>
-      <Card.Img variant="top" src={product.image} />
+      <Link to={`/p/${product.id}`}>
+        <Card.Img variant="top" src={product.image} />
 
-      <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
 
-        <Card.Text>€{product.price}</Card.Text>
+          <Card.Text>€{product.price}</Card.Text>
 
-        <Rating initialValue={product.rating.rate} readonly />
-
-        <Button variant="outline-primary" as={Link} to={`/p/${product.id}`}>
-          View Details
-        </Button>
-        <Button variant="success">Add To Cart</Button>
-      </Card.Body>
+          <Rating initialValue={product.rating.rate} readonly />
+        </Card.Body>
+      </Link>
     </Card>
   );
 }
