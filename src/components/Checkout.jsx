6c691d14496d43f706191b440fafe5adc2f64e09
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2024-02-02 18:37:13 +0100

    add Cart summary to Checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,16 +1,31 @@
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import CheckoutDetails from './CheckoutDetails';
+import { useStoreState } from 'easy-peasy';
 
 function Checkout() {
+  const getSumOfCart = useStoreState((state) => state.getSumOfCart);
+
   return (
     <section>
+      <h1>Checkout</h1>
+
       <Row>
         <Col md={8}>
           <CheckoutDetails />
         </Col>
 
         <Col md={4}>
-          <h1>Checkout</h1>
+          <Card>
+            <Card.Body>
+              <Card.Title>Summary</Card.Title>
+              <Card.Text>
+                Total (VAT included) <strong>{getSumOfCart.toFixed(2)} €</strong>
+              </Card.Text>
+              <Card.Text>
+                Shipping <strong>Free</strong>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </section>
