b5efc26b56a1aca65ae63bc902128d7f3ab59231
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-08-05 23:05:44 +0200

    add class to CARD

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import { Rating } from 'react-simple-star-rating';
+import '../styles/ProductCard.css';
 
 function ProductCard({ product }) {
   return (
-    <Card key={product.id}>
+    <Card className="product-card" key={product.id}>
       <Link to={`/p/${product.id}`}>
-        <Card.Img variant="top" src={product.image} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        <Card.Img variant="top" src={product.image} className="product-image" />
 
         <Card.Body>
           <Card.Title>{product.title}</Card.Title>
